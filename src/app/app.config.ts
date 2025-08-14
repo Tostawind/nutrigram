83fa4268343e91fac06d420788f4029442775548
Abel D. <abel.dieguez@wakasolutions.com>
2025-08-14 09:56:31 +0200

    feat: primeng & meals page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 
+// PRIME NG:
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    // PrimeNG
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
+  ],
 };
--- src/app/pages/meals/meals.component.ts
+++ src/app/pages/meals/meals.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-meals',
-  imports: [],
+  imports: [CardModule, RouterLink],
   templateUrl: './meals.component.html',
-  styleUrl: './meals.component.scss'
+  styleUrl: './meals.component.scss',
 })
 export class MealsComponent {
-
+  meals = [
+    { id: 1, name: '☀️ Desayuno' },
+    { id: 2, name: '🍽️ Comida' },
+    { id: 3, name: '🌕 Cena' },
+    { id: 4, name: '🍫 Snack' },
+    { id: 5, name: '🍌 Snack 2' },
+  ];
 }
