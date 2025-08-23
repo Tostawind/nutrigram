744742c05ecc109034a26d91acdb739856df719e
A.Wind <94459644+Tostawind@users.noreply.github.com>
2025-08-23 16:53:39 +0000

    wip: recipe form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { Toast } from 'primeng/toast';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabel } from 'primeng/floatlabel';
+import { FormsModule } from '@angular/forms';
+import { MealService } from './core/services/meal.service';
+import { Meal } from './core/models/meal.model';
+import { RadioButtonModule } from 'primeng/radiobutton';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, RouterLink, Toast],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    ButtonModule,
+    RouterLink,
+    Toast,
+    DialogModule,
+    InputTextModule,
+    FloatLabel,
+    RadioButtonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  private _mealService = inject(MealService);
+
+  visible: boolean = false;
+  value2: string = '';
+  meal: string = '';
+  meals: Meal[] = [];
+  selectedMeal: Meal | null = null;
+
+  ngOnInit() {
+    this.loadMeals();
+  }
+
+  async loadMeals() {
+    await this._mealService.getMeals();
+    this.meals = this._mealService.meals();
+  }
+}
