d11aa2537bc42995505ea74dbf3d731fbaff5da7
Abel D <adq21791@gmail.com>
2025-09-08 11:30:18 +0200

    feat: change app name to nutrigram

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'macros-app' title`, () => {
+  it(`should have the 'nutrigram' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('macros-app');
+    expect(app.title).toEqual('nutrigram');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, macros-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, nutrigram');
   });
 });
